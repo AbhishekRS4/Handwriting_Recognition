e11bfef8a047933bbbd3903e2376a0a6d832349d
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2022-05-12 23:12:42 +0200

    added computation and logging of validation metrics

--- line_recognition/train.py
+++ line_recognition/train.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 
 from model_crnn import CRNN
 from logger_utils import CSVWriter, write_json_file
+from utils import compute_wer_and_cer_for_batch, ctc_decode
 from dataset import HWRecogIAMDataset, split_dataset, get_dataloaders_for_training
 
 
@@ -18,20 +19,19 @@ def train(hw_model, optimizer, criterion, train_loader, device):
     num_train_samples = len(train_loader.dataset)
     num_train_batches = len(train_loader)
 
-    for images, labels, length_labels in train_loader:
+    for images, labels, lengths_labels in train_loader:
         images = images.to(device, dtype=torch.float)
         labels = labels.to(device, dtype=torch.long)
-        length_labels = length_labels.to(device, torch.long)
+        lengths_labels = lengths_labels.to(device, torch.long)
 
         batch_size = images.size(0)
         optimizer.zero_grad()
-        logits = hw_model(images)
-        log_probs = F.log_softmax(logits, dim=2)
+        log_probs = hw_model(images)
 
-        length_outputs = torch.LongTensor([logits.size(0)] * batch_size)
-        length_labels = torch.flatten(length_labels)
+        lengths_preds = torch.LongTensor([log_probs.size(0)] * batch_size)
+        lengths_labels = torch.flatten(lengths_labels)
 
-        loss = criterion(log_probs, labels, length_outputs, length_labels)
+        loss = criterion(log_probs, labels, lengths_preds, lengths_labels)
         train_running_loss += loss.item()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(hw_model.parameters(), 5) # gradient clipping with 5
@@ -43,25 +43,64 @@ def train(hw_model, optimizer, criterion, train_loader, device):
 def validate(hw_model, criterion, valid_loader, device):
     hw_model.eval()
     valid_running_loss = 0.0
+    valid_running_cer = 0.0
+    valid_running_wer = 0.0
     num_valid_samples = len(valid_loader.dataset)
     num_valid_batches = len(valid_loader)
 
+    count = 0
     with torch.no_grad():
-        for images, labels, length_labels in valid_loader:
+        for images, labels, lengths_labels in valid_loader:
             images = images.to(device, dtype=torch.float)
             labels = labels.to(device, dtype=torch.long)
-            length_labels = length_labels.to(device, torch.long)
+            lengths_labels = lengths_labels.to(device, torch.long)
 
             batch_size = images.size(0)
-            logits = hw_model(images)
-            log_probs = F.log_softmax(logits, dim=2)
-            length_outputs = torch.LongTensor([logits.size(0)] * batch_size)
+            log_probs = hw_model(images)
+            lengths_preds = torch.LongTensor([log_probs.size(0)] * batch_size)
 
-            loss = criterion(log_probs, labels, length_outputs, length_labels)
+            loss = criterion(log_probs, labels, lengths_preds, lengths_labels)
             valid_running_loss += loss.item()
 
+            pred_labels = ctc_decode(log_probs)
+            labels_for_eval = labels.cpu().numpy().tolist()
+            lengths_labels_for_eval = lengths_labels.cpu().numpy().tolist()
+
+            final_labels_for_eval = []
+            length_label_counter = 0
+            for pred_label, length_label in zip(pred_labels, lengths_labels_for_eval):
+                label = labels_for_eval[length_label_counter:length_label_counter+length_label]
+                length_label_counter += length_label
+
+                final_labels_for_eval.append(label)
+
+            """
+            print(len(final_labels_for_eval))
+            print(final_labels_for_eval)
+            print("")
+            print(len(pred_labels))
+            print(pred_labels)
+            print("")
+            """
+
+            for i in range(len(final_labels_for_eval)):
+                if len(pred_labels[i]) != 0:
+                    str_label = [HWRecogIAMDataset.LABEL_2_CHAR[i] for i in final_labels_for_eval[i]]
+                    str_label = "".join(str_label)
+                    str_pred = [HWRecogIAMDataset.LABEL_2_CHAR[i] for i in pred_labels[i]]
+                    str_pred = "".join(str_pred)
+
+                    cer_sample, wer_sample = compute_wer_and_cer_for_sample(str_pred, str_label)
+                else:
+                    cer_sample, wer_sample = 100, 100
+
+                valid_running_cer += cer_sample
+                valid_running_wer += wer_sample
+
         valid_loss = valid_running_loss / num_valid_batches
-    return valid_loss
+        valid_cer = valid_running_cer / num_valid_samples
+        valid_wer = valid_running_wer / num_valid_samples
+    return valid_loss, valid_cer, valid_wer
 
 def train_hw_recognizer(FLAGS):
     file_txt_labels = os.path.join(FLAGS.dir_dataset, "iam_lines_gt.txt")
@@ -91,7 +130,7 @@ def train_hw_recognizer(FLAGS):
     file_logger_train = os.path.join(dir_model, "train_metrics.csv")
     csv_writer = CSVWriter(
         file_name=file_logger_train,
-        column_names=["epoch", "loss_train", "loss_valid"]
+        column_names=["epoch", "loss_train", "loss_valid", "cer_valid", "wer_valid"]
     )
 
     file_params = os.path.join(dir_model, "params.json")
@@ -113,19 +152,21 @@ def train_hw_recognizer(FLAGS):
     for epoch in range(1, FLAGS.num_epochs+1):
         time_start = time.time()
         train_loss = train(hw_model, optimizer, criterion, train_loader, device)
-        valid_loss = validate(hw_model, criterion, valid_loader, device)
+        valid_loss, valid_cer, valid_wer = validate(hw_model, criterion, valid_loader, device)
         time_end = time.time()
         print(f"epoch: {epoch}/{FLAGS.num_epochs}, time: {time_end-time_start:.3f} sec.")
-        print(f"train loss: {train_loss:.6f}, validation loss: {valid_loss:.6f}\n")
+        print(f"train loss: {train_loss:.6f}, validation loss: {valid_loss:.6f}, validation cer: {valid_cer:.4f}, validation wer: {valid_wer:.4f}\n")
 
         csv_writer.write_row(
             [
                 epoch,
                 round(train_loss, 6),
                 round(valid_loss, 6),
+                round(valid_cer, 4),
+                round(valid_wer, 4),
             ]
         )
-        torch.save(hw_model.state_dict(), os.path.join(dir_model, f"{FLAGS.which_hw_model}_{epoch}.pth"))
+        torch.save(hw_model.state_dict(), os.path.join(dir_model, f"{FLAGS.which_hw_model}_H_{FLAGS.image_height}_W_{FLAGS.image_width}_E_{epoch}.pth"))
     print(f"Training of handwriting recognition model {FLAGS.which_hw_model} complete!!!!")
     csv_writer.close()
     return
