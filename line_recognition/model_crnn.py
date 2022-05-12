a94e5e23a8ef92c7ea5ce09f19f30fc5bd81cc14
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2022-05-12 23:11:48 +0200

    rejig crnn and rnn_seq2seq modules

--- line_recognition/model_crnn.py
+++ line_recognition/model_crnn.py
@@ -1,44 +1,55 @@
 import torchvision
 import torch.nn as nn
+import torch.nn.functional as F
 
 from model_visual_features import ResNetFeatureExtractor
 
-class CRNN(nn.Module):
-    def __init__(self, num_classes, image_height, num_feats_mapped_seq_hidden=128, num_feats_seq_hidden=256):
+class HW_RNN_Seq2Seq(nn.Module):
+    def __init__(self, num_classes, image_height, cnn_output_channels=512, num_feats_mapped_seq_hidden=128, num_feats_seq_hidden=256):
         super().__init__()
-        self.visual_feature_extractor = ResNetFeatureExtractor()
         self.output_height = image_height // 32
 
-        self.map_visual_to_seq = nn.Linear(self.visual_feature_extractor.output_channels * self.output_height, num_feats_mapped_seq_hidden)
+        self.map_visual_to_seq = nn.Linear(cnn_output_channels * self.output_height, num_feats_mapped_seq_hidden)
 
         self.b_lstm_1 = nn.LSTM(num_feats_mapped_seq_hidden, num_feats_seq_hidden, bidirectional=True)
         self.b_lstm_2 = nn.LSTM(2 * num_feats_seq_hidden, num_feats_seq_hidden, bidirectional=True)
 
         self.final_dense = nn.Linear(2 * num_feats_seq_hidden, num_classes)
 
+    def forward(self, visual_feats):
+        visual_feats = visual_feats.permute(3, 0, 1, 2)
+        # WBCH
+        # the sequence is along the width of the image as a sentence
+
+        visual_feats = visual_feats.contiguous().view(visual_feats.shape[0], visual_feats.shape[1], -1)
+        # WBC
+
+        seq = self.map_visual_to_seq(visual_feats)
+        lstm_1, _ = self.b_lstm_1(seq)
+        lstm_2, _ = self.b_lstm_2(lstm_1)
+
+        dense_output = self.final_dense(lstm_2)
+        # [seq_len, B, num_classes]
+
+        log_probs = F.log_softmax(dense_output, dim=2)
+
+        return log_probs
+
+
+class CRNN(nn.Module):
+    def __init__(self, num_classes, image_height, num_feats_mapped_seq_hidden=128, num_feats_seq_hidden=256):
+        super().__init__()
+        self.visual_feature_extractor = ResNetFeatureExtractor()
+        self.rnn_seq2seq_module = HW_RNN_Seq2Seq(num_classes, image_height, self.visual_feature_extractor.output_channels, num_feats_mapped_seq_hidden, num_feats_seq_hidden)
+
     def forward(self, x):
         visual_feats = self.visual_feature_extractor(x)
         # [B, 512, H/32, W/32]
 
-        batch_size, num_channels, height, width = visual_feats.size()
-        # BCHW
+        log_probs = self.rnn_seq2seq_module(visual_feats)
+        return log_probs
 
 
-        visual_feats = visual_feats.permute(3, 0, 1, 2)
-        visual_feats = visual_feats.contiguous().view(visual_feats.shape[0], visual_feats.shape[1], -1)
-
-        #visual_feats = visual_feats.view(batch_size, num_channels * height, width)
-        # [B, C*H, W]
-
-        #visual_feats = visual_feats.permute(2, 0, 1)
-        # [W, B, C*H]
-
-        seq = self.map_visual_to_seq(visual_feats)
-
-        lstm_1, _ = self.b_lstm_1(seq)
-        lstm_2, _ = self.b_lstm_2(lstm_1)
-
-        output = self.final_dense(lstm_2)
-        # [seq_len, B, num_classes]
-
-        return output
+class STN_CRNN(nn.Module):
+    def __init__(self):
+        print("Not yet implemented")
