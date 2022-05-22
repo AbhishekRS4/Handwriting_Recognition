ef889fa594b75038ac6194a200d7c0e6c142296a
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2022-05-22 19:10:02 +0200

    updated test script

--- line_recognition/test.py
+++ line_recognition/test.py
@@ -4,36 +4,51 @@ import time
 import torch
 import argparse
 import torchvision
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
-from model_crnn import CRNN
-from ctc_decoder import ctc_decode
+from model_main import CRNN
+from utils import ctc_decode, compute_wer_and_cer_for_sample
 from dataset import HWRecogIAMDataset, split_dataset, get_dataloader_for_testing
 
 
-def test(hw_model, test_loader, device):
+def test(hw_model, test_loader, device, list_test_files):
     hw_model.eval()
     num_test_samples = len(test_loader.dataset)
     num_test_batches = len(test_loader)
 
     count = 0
+    list_test_cers, list_test_wers = [], []
 
     with torch.no_grad():
         for images, labels, length_labels in test_loader:
-            if count == 5:
-                break
-            images = images.to(device, dtype=torch.float)
-
-            batch_size = images.size(0)
-            logits = hw_model(images)
-            log_probs = F.log_softmax(logits, dim=2)
-            pred_label = ctc_decode(log_probs)#, HWRecogIAMDataset.LABEL_2_CHAR)
-            print(labels.cpu().numpy().tolist(), pred_label)
             count += 1
+            images = images.to(device, dtype=torch.float)
+            log_probs = hw_model(images)
+            pred_labels = ctc_decode(log_probs)
+            labels = labels.cpu().numpy().tolist()
+            #print(labels, pred_labels[0])
+            str_label = [HWRecogIAMDataset.LABEL_2_CHAR[i] for i in labels]
+            str_label = "".join(str_label)
+            str_pred = [HWRecogIAMDataset.LABEL_2_CHAR[i] for i in pred_labels[0]]
+            str_pred = "".join(str_pred)
+
+            cer_sample, wer_sample = compute_wer_and_cer_for_sample(str_pred, str_label)
+            list_test_cers.append(cer_sample)
+            list_test_wers.append(wer_sample)
+            print(f"progress: {count}/{num_test_samples}, test file: {list_test_files[count-1]}")
+            print(f"{str_label} - label")
+            print(f"{str_pred} - prediction")
+            print(f"cer: {cer_sample:.3f}, wer: {wer_sample:.3f}\n")
+    list_test_cers = np.array(list_test_cers)
+    list_test_wers = np.array(list_test_wers)
+    mean_test_cer = np.mean(list_test_cers)
+    mean_test_wer = np.mean(list_test_wers)
+    print(f"test set - mean cer: {mean_test_cer:.3f}, mean wer: {mean_test_wer:.3f}\n")
     return
 
-def train_hw_recognizer(FLAGS):
+def test_hw_recognizer(FLAGS):
     file_txt_labels = os.path.join(FLAGS.dir_dataset, "iam_lines_gt.txt")
     dir_images = os.path.join(FLAGS.dir_dataset, "img")
     os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
@@ -57,12 +72,21 @@ def train_hw_recognizer(FLAGS):
     print(f"image height: {FLAGS.image_height}, image width: {FLAGS.image_width}")
     print(f"num test samples: {num_test_samples}")
 
-    hw_model = CRNN(num_classes, FLAGS.image_height)
+    if FLAGS.which_hw_model == "crnn":
+        hw_model = CRNN(num_classes, FLAGS.image_height)
+    elif FLAGS.which_hw_model == "stn_crnn":
+        hw_model = STN_CRNN(num_classes, FLAGS.image_height, FLAGS.image_width)
+    elif FLAGS.which_hw_model == "stn_pp_crnn":
+        hw_model = STN_PP_CRNN(num_classes, FLAGS.image_height, FLAGS.image_width)
+    else:
+        print(f"unidentified option : {FLAGS.which_hw_model}")
+        sys.exit(0)
     hw_model.to(device)
     hw_model.load_state_dict(torch.load(FLAGS.file_model))
 
-    print(f"testing of handwriting recognition model {FLAGS.which_hw_model} started")
-    test(hw_model, test_loader, device)
+    print(f"testing of handwriting recognition model {FLAGS.which_hw_model} started\n")
+    test(hw_model, test_loader, device, test_x)
+    print(f"testing handwriting recognition model completed!!!!")
     return
 
 def main():
@@ -83,12 +107,12 @@ def main():
     parser.add_argument("--dir_dataset", default=dir_dataset,
         type=str, help="full directory path to the dataset")
     parser.add_argument("--which_hw_model", default=which_hw_model,
-        type=str, choices=["crnn"], help="which model to train")
+        type=str, choices=["crnn", "stn_crnn", "stn_pp_crnn"], help="which model to train")
     parser.add_argument("--file_model", default=file_model,
         type=str, help="full path to trained model file (.pth)")
 
     FLAGS, unparsed = parser.parse_known_args()
-    train_hw_recognizer(FLAGS)
+    test_hw_recognizer(FLAGS)
     return
 
 if __name__ == "__main__":
