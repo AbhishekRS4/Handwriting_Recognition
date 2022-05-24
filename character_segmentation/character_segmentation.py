039b13ee281cb54348de1a61fb5b5551a70e54f4
Jeroen <jeroen.h.vangelder@gmail.com>
2022-05-24 12:34:03 +0200

    small changes

--- character_segmentation/character_segmentation.py
+++ character_segmentation/character_segmentation.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal as sp
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,12 +14,8 @@ def get_histogram(file):
     im_transp = image.T
     bins = []
 
-    print(np.unique(im_transp))
-
     for i in range(len(im_transp)):
         bins.append(np.sum(im_transp[i] == 0))
-
-    print(len(bins))
 
     fig = plt.figure()
 
@@ -33,13 +30,16 @@ def get_histogram(file):
     # Plot graph
     plt.title('histogram ')
     plt.stairs(bins, fill=True)
-    # plt.bar(range(len(bins)), bins, width=1.0)
-    print(max(bins))
-    print(np.argmax(bins))
+
+    peaks, _ = sp.find_peaks(bins)
+    maxima = np.array(bins)[peaks.astype(int)]
+
+    plt.plot(peaks, maxima, "x")
+    plt.vlines(x=peaks, ymin=0, ymax=maxima, colors="red")
 
     plt.show()
 
 
 if __name__ == "__main__":
-    file = os.path.join(ROOT_DIR, 'line_segmentation\crops\image_1_crop_1.jpg')
+    file = os.path.join(ROOT_DIR, 'line_segmentation\crops\image_1_crop_8.jpg')
     get_histogram(file)
