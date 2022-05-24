5e9e00946bc880aa2b844e487ea19c365d379e1b
Jeroen <jeroen.h.vangelder@gmail.com>
2022-05-24 12:03:27 +0200

    Now actually black and white image...

--- character_segmentation/character_segmentation.py
+++ character_segmentation/character_segmentation.py
@@ -9,8 +9,11 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def get_histogram(file):
     image = cv2.imread(file)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     im_transp = image.T
     bins = []
+
+    print(np.unique(im_transp))
 
     for i in range(len(im_transp)):
         bins.append(np.sum(im_transp[i] == 0))
@@ -29,7 +32,10 @@ def get_histogram(file):
 
     # Plot graph
     plt.title('histogram ')
-    plt.bar(range(len(bins)), bins, width=1.0)
+    plt.stairs(bins, fill=True)
+    # plt.bar(range(len(bins)), bins, width=1.0)
+    print(max(bins))
+    print(np.argmax(bins))
 
     plt.show()
 
@@ -37,4 +43,3 @@ def get_histogram(file):
 if __name__ == "__main__":
     file = os.path.join(ROOT_DIR, 'line_segmentation\crops\image_1_crop_1.jpg')
     get_histogram(file)
-    
