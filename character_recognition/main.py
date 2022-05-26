4f247618bf4e4d280bcf628c23ce8b03963cfba2
Savoyevatel <71535298+Savoyevatel@users.noreply.github.com>
2022-05-26 22:41:13 +0200

    Add files via upload
    cleaned the data

---
+++ character_recognition/main.py
@@ -0,0 +1,92 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+%matplotlib inline
+import train
+import model
+import train
+
+pretrain = True
+batch_size= 32
+input_size = (64, 64)
+optimizer = 'adam'
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(physical_devices))
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+
+path_train_data ="char_recog/train"
+path_test_data ="char_recog/val"
+
+import splitfolders
+
+splitfolders.ratio('char_training', output="char_recog", seed=1337, ratio=(0.8, 0.2))
+
+subfolder =["train", "val"]
+for sf in subfolder:
+    mainfolder= f"char_recog/{sf}"
+    folders = os.listdir(mainfolder)
+    for folder in  folders:
+        files = os.listdir(f"{mainfolder}/{folder}")
+        for f in files:
+            impath = f"{mainfolder}/{folder}/{f}"
+            newfile = "".join(f.split(".")[0:-1])
+            newpath = f"{mainfolder}/{folder}/{newfile}.png"
+            img = Image.open(impath).convert("RGBA")
+            img.save(newpath)
+
+
+def char_recog():
+    model= model.alexnet(27, (64,64,3),"adam")
+    print("Now loading pretrained weights.....")
+    model.load_weights("f"{name}.h5".h5")
+
+if __name__ == '__main__':
+    input_shape = (64, 64, 3)
+    optimizer = 'adam'
+    # (1) Create a model
+    model= model.alexnet(27, (64,64,3),"adam")
+    # (2) Compile
+    #model.compile(loss=keras.losses.categorical_crossentropy, optimizer=optimizer, metrics=["accuracy"])
+    
+
+    # (3) Train
+    if pretrain ==True: 
+        print("Pretraining")
+        pre_history = train.training("alexnet", (64, 64), 32)
+        print("pretrain done")
+    history = Fit_model.fitting_model(model, 'cnnmodel-' + optimizer, (64, 64))
+    
+    # (4) Print results
+    if pretrain: 
+        print(pre_history.history) 
+    
+    print(history.history)
+    plt.plot(history.history['accuracy'])
+    plt.title('Alexnet')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train'], loc='upper left')
+    plt.show()
---
+++ character_recognition/train.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+%matplotlib inline
+import splitfolders
+
+
+def training(name,input_size, batch_size):
+    gen_train_data = ImageDataGenerator(rescale=1./255, width_shift_range=0.1,
+        shear_range=0.15, zoom_range=0.1,
+        channel_shift_range=10., horizontal_flip=False)
+
+    gen_test_data = ImageDataGenerator(rescale=1./255)
+
+
+    train_generator = gen_train_data.flow_from_directory(
+                path_train_data, 
+                target_size=input_size, 
+                batch_size=batch_size,
+                class_mode='categorical')
+
+    val_generator = gen_train_data.flow_from_directory(
+                path_test_data,  # this is the target directory
+                target_size=input_size,  # all images will be resized to 64x64
+                batch_size=batch_size,
+                class_mode='categorical',subset='training')
+
+#reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=2, min_lr=0.001)
+
+    weights_pretrain = model.fit_generator(
+        train_generator,
+        steps_per_epoch= train_generator.samples // batch_size,
+        epochs=100,
+        validation_data=val_generator,
+        validation_steps=val_generator.samples // batch_size)
+    model.save_weights(f"{name}.h5")
+    return weights_pretrain
