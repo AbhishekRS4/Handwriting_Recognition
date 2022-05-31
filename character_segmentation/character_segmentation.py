02602d9086819603e637521229972e295894f2c6
Jeroen <jeroen.h.vangelder@gmail.com>
2022-05-31 15:57:06 +0200

    Initial implementation of advanced histogram based method

--- character_segmentation/character_segmentation.py
+++ character_segmentation/character_segmentation.py
@@ -18,24 +18,120 @@ def trim_sides(image):
 
 
 # this function takes the locations of the binarized valleys and splits the image on these points
-def split_characters(image, splits):
+def split_characters(image, peaks, widths):
     characters = []
 
-    min = 0
-    print(len(image.T))
-    for split in splits:
-        characters.append(image[:, min:split])
-        min = split
+    for i in range(len(peaks)):
+        temp = image[:, (peaks[i] - widths[i] / 2).astype(int):(peaks[i] + widths[i] / 2).astype(int)]
+        characters.append(temp)
 
-        if split == splits[-1]:
-            characters.append(image[:, min:len(image.T)])
+        cv2.imshow("split", temp)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return characters
+
+# case 1: join successive zero's
+# case 2: join zero between ones to the closest one
+def temp_binary_joining_rule(peaks, widths, binary):
+    i = 1
+    first_zero = 0 if binary[0] == 0 else None
+
+    while i < len(binary) - 1:
+        left_zero = True if first_zero else False
+        right_zero = True if binary[i + 1] == 0 else False
+
+        if binary[i] == 0:
+            if not left_zero and right_zero:
+                first_zero = i
+            elif left_zero and not right_zero:
+                new_width = (peaks[i] + widths[i] / 2) - (peaks[first_zero] - widths[first_zero] / 2)
+                new_peak = peaks[first_zero] - widths[first_zero] / 2 + new_width / 2
+
+                binary = np.delete(binary, np.arange(first_zero, i+1))
+                widths = np.delete(widths, np.arange(first_zero, i+1))
+                peaks = np.delete(peaks, np.arange(first_zero, i+1))
+
+                binary = np.insert(binary, first_zero, 1)
+                widths = np.insert(widths, first_zero, new_width)
+                peaks = np.insert(peaks, first_zero, new_peak)
+
+                i = first_zero
+                first_zero = None
+            elif not left_zero and not right_zero:
+                if peaks[i-1] + widths[i-1] / 2 < peaks[i+1] - widths[i+1] / 2:
+                    new_width = (peaks[i] + widths[i] / 2) - (peaks[i-1] - widths[i-1] / 2)
+                    new_peak = peaks[i-1] - widths[i-1] / 2 + new_width / 2
+
+                    binary = np.delete(binary, np.arange(i-1, i+1))
+                    widths = np.delete(widths, np.arange(i-1, i+1))
+                    peaks = np.delete(peaks, np.arange(i-1, i+1))
+
+                    binary = np.insert(binary, i-1, 1)
+                    widths = np.insert(widths, i-1, new_width)
+                    peaks = np.insert(peaks, i-1, new_peak)
+
+                    i -= 1
+                    first_zero = None
+                else:
+                    new_width = (peaks[i+1] + widths[i+1] / 2) - (peaks[i] - widths[i] / 2)
+                    new_peak = peaks[i] - widths[i] / 2 + new_width / 2
+
+                    binary = np.delete(binary, np.arange(i, i+2))
+                    widths = np.delete(widths, np.arange(i, i+2))
+                    peaks = np.delete(peaks, np.arange(i, i+2))
+
+                    binary = np.insert(binary, i, 1)
+                    widths = np.insert(widths, i, new_width)
+                    peaks = np.insert(peaks, i, new_peak)
+
+                    first_zero = None
+        i += 1
+
+    if binary[i] == 0 and binary[i-1] == 0:
+        new_width = (peaks[i] + widths[i] / 2) - (peaks[first_zero] - widths[first_zero] / 2)
+        new_peak = peaks[first_zero] - widths[first_zero] / 2 + new_width / 2
+
+        widths = np.delete(widths, np.arange(first_zero, i+1))
+        peaks = np.delete(peaks, np.arange(first_zero, i+1))
+
+        widths = np.insert(widths, first_zero, new_width)
+        peaks = np.insert(peaks, first_zero, new_peak)
+
+    return peaks, widths
+
+
+# this function binarizes peaks and joins peaks based on relative with according to research by
+# Rajput, Jayanthi and Sreedevi (2019)
+def segment_characters(peaks, widths, image):
+    k = 0.7
+
+    # compute average width
+    width_avg = np.mean(widths)
+
+    binary = []
+
+    # label peaks
+    for width in widths:
+        if width < k * width_avg:
+            binary.append(0)
+        else:
+            binary.append(1)
+
+    print(binary)
+
+    # TODO: incorporate max distance between peaks to join
+
+    new_peaks, new_widths = temp_binary_joining_rule(peaks, widths, binary)
+    print(new_peaks, new_widths)
+
+    characters = split_characters(image, new_peaks, new_widths)
 
     return characters
 
 
-
 # this function creates a histogram from the input image and determines the splits based on the binarized histogram
-def get_histogram(file):
+def apply_histogram_segmentation(file):
     image = cv2.imread(file)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
@@ -63,6 +159,7 @@ def get_histogram(file):
 
     # Plot binary graph
     binarized = (bins > np.mean(bins)).astype(np.int_)
+    # binarized = (bins > np.mean(bins)/2).astype(np.int_)
 
     ax[2].set_title(' binarized ')
     ax[2].stairs(binarized, fill=False, color="orange")
@@ -72,15 +169,21 @@ def get_histogram(file):
 
     plt.show()
 
-    characters = split_characters(image, binary_valleys)
+    # characters = split_characters_at_valley(image, binary_valleys)
 
-    for character in characters:
-        cv2.imshow("char", character)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    binary_peaks, _ = sp.find_peaks(binarized)
+    widths = sp.peak_widths(binarized, binary_peaks, rel_height=1)
+
+    characters = segment_characters(binary_peaks, widths[0], image)
+
+    # for character in characters:
+    #     cv2.imshow("char", character)
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
     # file = os.path.join(ROOT_DIR, 'line_segmentation\crops\image_1_crop_8.jpg')
-    file = os.path.join(ROOT_DIR, 'line_segmentation\crops\image_1_crop_6.jpg')
-    get_histogram(file)
+    # file = os.path.join(ROOT_DIR, 'line_segmentation\crops\image_1_crop_6.jpg')
+    file = os.path.join(ROOT_DIR, 'line_segmentation\crops\image_2_crop_10.jpg')
+    apply_histogram_segmentation(file)
