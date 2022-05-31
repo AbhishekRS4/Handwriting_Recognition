be777c15662ee6a4b15e99282c25931ff9af41e9
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2022-05-31 14:32:15 +0200

    added comments to dataset script

--- line_recognition/dataset.py
+++ line_recognition/dataset.py
@@ -9,6 +9,22 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 
 def read_IAM_label_txt_file(file_txt_labels):
+    """
+    ---------
+    Arguments
+    ---------
+    file_txt_labels : str
+        full path to the text file containing labels
+
+    -------
+    Returns
+    -------
+    a tuple of
+    all_image_files : list
+        a list of all image file names
+    all_labels : list
+        a list of all labels
+    """
     label_file_handler = open(file_txt_labels, mode="r")
     all_lines = label_file_handler.readlines()
     num_lines = len(all_lines)
@@ -27,14 +43,34 @@ def read_IAM_label_txt_file(file_txt_labels):
     return all_image_files, all_labels
 
 class HWRecogIAMDataset(Dataset):
+    """
+    Main dataset class to be used only for training, validation and internal testing
+    """
     CHAR_SET = ' !"#&\'()*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     CHAR_2_LABEL = {char: i + 1 for i, char in enumerate(CHAR_SET)}
     LABEL_2_CHAR = {label: char for char, label in CHAR_2_LABEL.items()}
 
-    def __init__(self, image_files, labels, dir_images, image_height=32, image_width=768, which_set="train"):
-        self.labels = labels
+    def __init__(self, list_image_files, list_labels, dir_images, image_height=32, image_width=768, which_set="train"):
+        """
+        ---------
+        Arguments
+        ---------
+        list_image_files : list
+            list of image files
+        list_labels : list
+            list of labels
+        dir_images : str
+            full path to directory containing images
+        image_height : int
+            image height (default: 32)
+        image_width : int
+            image width (default: 768)
+        which_set : str
+            a string indicating which set is being used (default: train)
+        """
+        self.list_labels = list_labels
         self.dir_images = dir_images
-        self.image_files = image_files
+        self.list_image_files = list_image_files
         self.image_width = image_width
         self.image_height = image_height
         self.which_set = which_set
@@ -65,15 +101,15 @@ class HWRecogIAMDataset(Dataset):
             ])
 
     def __len__(self):
-        return len(self.image_files)
+        return len(self.list_image_files)
 
     def __getitem__(self, idx):
-        image_file_name = self.image_files[idx]
+        image_file_name = self.list_image_files[idx]
         image_gray = imread(os.path.join(self.dir_images, image_file_name))
         image_3_channel = np.repeat(np.expand_dims(image_gray, -1), 3, -1)
         image_3_channel = self.transform(image_3_channel)
 
-        label_string = self.labels[idx]
+        label_string = self.list_labels[idx]
         label_encoded = [self.CHAR_2_LABEL[c] for c in label_string]
         label_length = [len(label_encoded)]
 
@@ -83,6 +119,26 @@ class HWRecogIAMDataset(Dataset):
         return image_3_channel, label_encoded, label_length
 
 def IAM_collate_fn(batch):
+    """
+    collate function
+
+    ---------
+    Arguments
+    ---------
+    batch : tuple
+        a batch of input data as a tuple
+
+    -------
+    Returns
+    -------
+    a collated tuple of
+    images : tensor
+        tensor of batch images
+    labels : tensor
+        tensor of batch labels
+    label_lengths : tensor
+        tensor of batch label lengths
+    """
     images, labels, label_lengths = zip(*batch)
     images = torch.stack(images, 0)
     labels = torch.cat(labels, 0)
@@ -90,6 +146,20 @@ def IAM_collate_fn(batch):
     return images, labels, label_lengths
 
 def split_dataset(file_txt_labels, for_train=True):
+    """
+    ---------
+    Arguments
+    ---------
+    file_txt_labels : str
+        full path to the text file containing labels
+    for_train : bool
+        indicating whether split is for training or internal testing
+
+    -------
+    Returns
+    -------
+    a tuple of files depending for train or internal testing
+    """
     all_image_files, all_labels = read_IAM_label_txt_file(file_txt_labels)
     train_image_files, test_image_files, train_labels, test_labels = train_test_split(all_image_files, all_labels, test_size=0.1, random_state=4)
     train_image_files, valid_image_files, train_labels, valid_labels = train_test_split(train_image_files, train_labels, test_size=0.1, random_state=4)
@@ -99,6 +169,36 @@ def split_dataset(file_txt_labels, for_train=True):
         return test_image_files, test_labels
 
 def get_dataloaders_for_training(train_x, train_y, valid_x, valid_y, dir_images, image_height=32, image_width=768, batch_size=8):
+    """
+    ---------
+    Arguments
+    ---------
+    train_x : list
+        list of train file names
+    train_y : list
+        list of train labels
+    valid_x : list
+        list of validation file names
+    valid_y : list
+        list of validation labels
+    dir_images : str
+        full directory path containing the images
+    image_height : int
+        image height (default: 32)
+    image_width : int
+        image width (default: 768)
+    batch_size : int
+        batch size (default: 8)
+
+    -------
+    Returns
+    -------
+    a tuple of dataloaders objects
+    train_loader : object
+        object of train set dataloader
+    valid_loader : object
+        object of validation set dataloader
+    """
     train_dataset = HWRecogIAMDataset(train_x, train_y, dir_images, image_height=image_height, image_width=image_width, which_set="train")
     valid_dataset = HWRecogIAMDataset(valid_x, valid_y, dir_images, image_height=image_height, image_width=image_width, which_set="valid")
 
@@ -119,6 +219,29 @@ def get_dataloaders_for_training(train_x, train_y, valid_x, valid_y, dir_images,
     return train_loader, valid_loader
 
 def get_dataloader_for_testing(test_x, test_y, dir_images, image_height=32, image_width=768, batch_size=1):
+    """
+    ---------
+    Arguments
+    ---------
+    test_x : list
+        list of test file names
+    test_y : list
+        list of test labels
+    dir_images : str
+        full directory path containing the images
+    image_height : int
+        image height (default: 32)
+    image_width : int
+        image width (default: 768)
+    batch_size : int
+        batch size (default: 1)
+
+    -------
+    Returns
+    -------
+    test_loader : object
+        object of test set dataloader
+    """
     test_dataset = HWRecogIAMDataset(test_x, test_y, dir_images=dir_images, image_height=image_height, image_width=image_width, which_set="test")
     test_loader = DataLoader(
         test_dataset,
